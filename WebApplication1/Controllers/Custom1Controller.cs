09e8b449d21f3a91263c90f0522f5f73dfc15565
Gaurav <gauravsuryawanshi191@gmail.com>
2023-12-16 22:14:00 +0530

    day10

---
+++ WebApplication1/Controllers/Custom1Controller.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Custom1Controller : Controller
+    {
+        public IActionResult MyPage()
+        {
+            return View();
+            //return Content("<script>alert('Welcome To All');</script>");
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
