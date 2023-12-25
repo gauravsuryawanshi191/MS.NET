c96f3969517e766b5eee47a75343564df3a2e365
Gaurav <gauravsuryawanshi191@gmail.com>
2023-12-25 23:04:30 +0530

    lab-eva

---
+++ StudentMVC/Controllers/StudentsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentMVC.Models;
+
+namespace StudentMVC.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: StudentsController
+        public ActionResult Index()
+        {
+            List<Student> students = new List<Student>();
+            students= Student.getAllStudents();
+            return View(students);
+        }
+
+        // GET: StudentsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student studentObj)
+        {
+            try
+            {
+                Student.saveStudent(studentObj);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+
+        // GET: StudentsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Student studentObj= Student.getStudentDetails(id);
+            return View(studentObj);
+        }
+
+        // POST: StudentsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                Student.deleteStudent(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        //// GET: StudentsController/Details/5
+        //public ActionResult Details(int id)
+        //{
+        //    return View();
+        //}
+
+        //// GET: StudentsController/Edit/5
+        //public ActionResult Edit(int id)
+        //{
+        //    return View();
+        //}
+
+        //// POST: StudentsController/Edit/5
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit(int id, IFormCollection collection)
+        //{
+        //    try
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    catch
+        //    {
+        //        return View();
+        //    }
+        //}
+    }
+}
---
+++ StudentMVC/Models/Student.cs
@@ -0,0 +1,142 @@
+﻿using Humanizer;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.CodeAnalysis.Elfie.Serialization;
+using Microsoft.Data.SqlClient;
+using NuGet.Protocol.Plugins;
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+using System.Drawing;
+
+using System.Net;
+using System.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace StudentMVC.Models
+{
+    public class Student
+    {
+        [Key]
+        [DisplayName("StudentId")]
+        public int StudentNo { get; set; }
+        [Required(ErrorMessage = "Name Required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Section Required")]
+        public string Section { get; set; }
+        [Required(ErrorMessage = "Branch Required")]
+        public string Branch { get; set; }
+        [Required(ErrorMessage ="Email Required")]
+        public string EmailId { get; set; } 
+
+        public static void saveStudent(Student obj)
+        {
+            using (SqlConnection cn = new SqlConnection()){
+                cn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=StudentMVCDB;Integrated Security=True";
+                try
+                {
+                    cn.Open();
+                    SqlCommand cmd = cn.CreateCommand();
+                    cmd.Connection = cn;
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Parameters.AddWithValue("@Name", obj.Name);
+                    cmd.Parameters.AddWithValue("@Section", obj.Section);
+                    cmd.Parameters.AddWithValue("@Branch", obj.Branch);
+                    cmd.Parameters.AddWithValue("@EmailId", obj.EmailId);
+                    cmd.CommandText = "insert into Students(Name,Section,Branch,EmailId) values (@Name,@Section,@Branch,@EmailId)";
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }     
+        }
+        public static List<Student> getAllStudents()
+        {
+            List<Student> studentList = new List<Student>();
+            using (SqlConnection cn = new SqlConnection())
+            {
+                cn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=StudentMVCDB;Integrated Security=True";
+                try
+                {
+                    cn.Open();
+                    SqlCommand cmd = cn.CreateCommand();
+                    cmd.Connection = cn;
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.CommandText = "select * from Students";
+                    SqlDataReader dr= cmd.ExecuteReader();
+                    while(dr.Read())
+                    {
+                        studentList.Add(new Student
+                        {
+                            StudentNo = dr.GetInt32("StudentNo"),
+                            Name = dr.GetString("Name"),
+                            Section = dr.GetString("Section"),
+                            Branch = dr.GetString("Branch"),
+                            EmailId= dr.GetString("EmailId")
+                        });
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+            return studentList;
+        }
+        public static Student getStudentDetails(int id)
+        {
+            Student student = new Student();
+            using (SqlConnection cn = new SqlConnection())
+            {
+                cn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=StudentMVCDB;Integrated Security=True";
+                try
+                {
+                    cn.Open();
+                    SqlCommand cmd = cn.CreateCommand();
+                    cmd.Connection = cn;
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.CommandText = "select * from Students";
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        student.StudentNo = dr.GetInt32("StudentNo");
+                        student.Name = dr.GetString("Name");
+                        student.Section = dr.GetString("Section");
+                        student.Branch = dr.GetString("Branch");
+                        student.EmailId = dr.GetString("EmailId");
+                }
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+            return student;
+
+        }
+        public static void deleteStudent(int StudentNo)
+        {
+            using (SqlConnection cn = new SqlConnection())
+            {
+                cn.ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=StudentMVCDB;Integrated Security=True";
+                try
+                {
+                    cn.Open();
+                    SqlCommand cmd = cn.CreateCommand();
+                    cmd.Connection = cn;
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Parameters.AddWithValue("@StudentNo", StudentNo);
+                    cmd.CommandText = "delete Students where StudentNo=@StudentNo";
+                    cmd.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
+
